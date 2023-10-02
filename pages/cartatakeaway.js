572687b042d8b25ec5be2c6f9c1d12317264705d
Ga Mayer <mayergabriela@gmail.com>
2023-10-02 17:42:58 -0300

    Footer styles were improved, Footer logo was added. WhatsApp logo was placed in the correct place. Footer Buttons were put in the correct place, Margin between the buttons were also added. Carta Take Away page was created and Heading was added.

---
+++ pages/cartatakeaway.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Cartatakeaway = () => {
+  return (
+    <>
+      <div>
+        <h2>Carta take away</h2>
+        <h3>Hacé tu pedido</h3>
+        <p>+54 1154253329</p>
+        <h3>Medios de pago: </h3>
+        <h4>Efectivo, Tarjetas Todas y Mercado Pago</h4>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default Cartatakeaway;
--- pages/components/FooterButton.js
+++ pages/components/FooterButton.js
@@ -4,19 +4,27 @@ const FooterButton = () => {
   return (
     <div>
       <div>
-        <button className="px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5  ">
+        <button className="my-3 px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5  ">
           Acerca de
         </button>
-        <button className="px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
+      </div>
+      <div>
+        <button className="my-3 px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
           Reservas
         </button>
-        <button className="px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
+      </div>
+      <div>
+        <button className="my-3 px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
           CARTA ONLINE
         </button>
-        <button className="px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
+      </div>
+      <div>
+        <button className="my-3 px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
           CARTA TAKE AWAY
         </button>
-        <button className="px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
+      </div>
+      <div>
+        <button className="my-3 px-5 duration-300 footerButton hover:bg-yellow-500 py-2.5 ">
           Contacto
         </button>
       </div>
