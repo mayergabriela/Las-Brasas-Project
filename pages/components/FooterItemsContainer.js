7e8eb93e879285d78e8f6e2261982bfa8307ec36
facu <facundo19burgos@gmail.com>
2023-10-04 16:37:50 -0300

    update

--- pages/components/FooterItemsContainer.js
+++ pages/components/FooterItemsContainer.js
@@ -5,7 +5,6 @@ import FooterImage from "./FooterImage";
 import FooterMenus from "./FooterMenus";
 
 const FooterItemsContainer = () => {
-  const year = new Date().getFullYear();
   return (
     <div className="grid grid-cols-1 gap-6 px-5 py-16 sm:grid-cols-3 lg:grid-cols-4 sm:px-8">
       <FooterImage />
@@ -13,7 +12,7 @@ const FooterItemsContainer = () => {
       <FooterItem />
       <FooterButton />
       <div className="grid-cols-1 pt-2 pb-8 text-sm text-center col-span4 bottom-color-footer">
-        <p>{year} Las Brasas Cook ⓒ | All Rights Reserved </p>
+        <p>2023 Las Brasas Cook ⓒ | All Rights Reserved </p>
       </div>
     </div>
   );
