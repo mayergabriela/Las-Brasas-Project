4c6a2bbc0d0c7da18e5808178149c48acb354783
Florencia <biasiflorenciaf@gmail.com>
2023-10-05 07:39:48 -0300

    Se agregó Componente Carousel con sus respectivas imágenes y se modificó aspecto de iconos en Navbar responsive

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import Footer from "./components/Footer.js";
 import Card from "./components/Card/Card.js";
 import Navbar from "./components/Navbar/Navbar.js";
 import Hero from "./components/Hero/Hero.js";
+import Carousel from "./components/Carousel/Carousel.js";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Navbar />
       <Hero />
+      <Carousel />
       <Card />
 
       <Footer />
