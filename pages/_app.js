a8ba54bcbf8b29284aff6d77f76dda97e0e445cc
facu <facundo19burgos@gmail.com>
2023-10-05 20:25:39 -0300

    ordene los archivos cambiando la ruta de footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "/styles/globals.css";
-import Footer from "./components/Footer.js";
+import Footer from "./components/footer/Footer";
 import Navbar from "./components/Navbar/Navbar.js";
 export default function App({ Component, pageProps }) {
   return (
--- pages/components/footer/FooterImage.js
+++ pages/components/footer/FooterImage.js
@@ -3,10 +3,11 @@ import logo from 'public/images/logo.png'
  
 const FooterImage = () => {
   return (
-    <div className="grid place-content-center">
+    <div className="grid place-content-center w-[200px]">
       <Image
       src={logo}
       alt="logo"
+      layout="responsive"
       priority
       />
     </div>
