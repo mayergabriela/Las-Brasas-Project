6c2ac4fe3179292f1a31e11ef3012f081af7c640
facu <facundo19burgos@gmail.com>
2023-10-05 13:39:40 -0300

    update app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import "@/styles/globals.css";
 import Footer from "./components/Footer.js";
 import Navbar from "./components/Navbar/Navbar.js";
-import Hero from "./components/Hero/Hero.js";
-import Carousel from "./components/Carousel/Carousel.js";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Navbar />
-      <Hero />
-      <Carousel />
+      <main></main>
       <Footer />
     </>
   );
--- pages/components/Carousel/Carousel.js
+++ pages/components/Carousel/Carousel.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
 import Image from 'next/image';
 
