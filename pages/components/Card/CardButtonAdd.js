613889993a28b70cde63006dda534538f940b224
Ga Mayer <mayergabriela@gmail.com>
2023-10-06 12:34:12 -0300

    Card component was modified. Card Button from Card was modified. Styles were changed. CardButtonAdd was mofidied. Color button was modified.

--- pages/components/Card/CardButtonAdd.js
+++ pages/components/Card/CardButtonAdd.js
@@ -3,7 +3,7 @@ import React from "react";
 const CardButtonAdd = () => {
   return (
     <>
-      <button className="mt-4 font-semibold text-center text-white duration-300 ease-out rounded-lg hover:bg-green-700 opacity-40 footerButton ">
+      <button className="mt-4 font-semibold text-center text-white duration-300 ease-out rounded-lg opacity-70 footerButton ">
         Agregar al carrito
       </button>
 
@@ -13,6 +13,10 @@ const CardButtonAdd = () => {
           text-transform: uppercase;
           box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
           cursor: pointer;
+        }
+        button:hover {
+          background-color: #2c2c2c;
+          color: var(--first-color);
         }
       `}</style>
     </>
