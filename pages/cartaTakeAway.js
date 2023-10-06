d3868c271b5ca85b4fdfa8522143d4ac0569438c
Ga Mayer <mayergabriela@gmail.com>
2023-10-06 15:33:38 -0300

    Update

---
+++ pages/cartaTakeAway.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Bannertakeaway from "./components/Take away/Bannertakeaway";
+import Card from "./components/Card/Card";
+
+const CartaTakeAway = () => {
+  return (
+    <div className="mt-20">
+      <Bannertakeaway />
+      <Card />
+    </div>
+  );
+};
+
+export default CartaTakeAway;
