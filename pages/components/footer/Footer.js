a197fb7f5a877d8ed0675f6c589134cfef159ad6
facu <facundo19burgos@gmail.com>
2023-10-05 21:35:10 -0300

    update

--- pages/components/footer/Footer.js
+++ pages/components/footer/Footer.js
@@ -3,7 +3,7 @@ import FooterItemsContainer from "./FooterItemsContainer";
 function Footer() {
   return (
     <footer className="text-white bg-yellow-500 ">
-      <div className="px-4 text-center md:flex md:justify-between md:items-center sm:px-12 py-7 md">
+      <div className="px-4 text-center md:items-center  py-7 md">
         <FooterItemsContainer />
       </div>
     </footer>
