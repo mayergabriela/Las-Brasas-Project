92259f55e6045c0948127732edd83b8582e89638
Ga Mayer <mayergabriela@gmail.com>
2023-10-08 05:15:03 -0300

    Cree loading en el CSS y le di estilos, Agregue el loader en la pagina Carta Take Away para poder visualizarlo. Cree el componente Loader y lo coloque en la carpeta Take Away

--- pages/cartaTakeAway.js
+++ pages/cartaTakeAway.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Bannertakeaway from "./components/Take away/Bannertakeaway";
 import Card from "./components/Card/Card";
+import Loader from "./components/Take away/Loader";
 
 const CartaTakeAway = () => {
   return (
     <section>
       <Bannertakeaway />
       <Card />
+      <Loader />
     </section>
   );
 };
