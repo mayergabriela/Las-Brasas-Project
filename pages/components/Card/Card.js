41426dba8343b4454ba0ff977d4dab9d60d77e41
Eduardo Coyto <eduardocoyto@gmail.com>
2023-10-08 10:04:35 -0300

    Se agregan el resto de los componentes que hacen a los productos. Resta dar estilo a las card para que queden todas iguales

--- pages/components/Card/Card.js
+++ pages/components/Card/Card.js
@@ -2,19 +2,37 @@
 import { useState, useEffect } from "react";
 import CardButton from "./CardButton";
 import CardButtonAdd from "./CardButtonAdd";
-//import Image from "next/image";
+import Postre from "../Products/Postre";
+import Acompaniamiento from "../Products/Acompaniamiento";
+import Cafeteria from "../Products/Cafeteria";
+import Principal from "../Products/Principal";
+import Infantil from "../Products/Infantil";
+import Cervezas from "../Products/Cervezas";
+import Vinos from "../Products/Vinos";
+import Bebidas from "../Products/Bebidas";
+
 
 const API = "https://brasas-cook-numen.onrender.com";
+const initial = {
+  acompaniamiento: [],
+  postre: [],
+  cafeteria: [],
+  bebidas: [],
+  cervezas: [],
+  vinos: [],
+  menu_infantil: [],
+  menu_principal: [],
+};
 
 function Card() {
-  const [product, setProduct] = useState({ postre: [] });
-  console.log(product);
+  const [product, setProduct] = useState(initial);
+  console.log(product.cafeteria.image);
   useEffect(() => {
     try {
       fetch(`${API}/db`)
         .then((response) => response.json())
         .then((data) => {
-          //console.log(data.acompaniamiento);
+          //console.log(data);
           setProduct(data);
         });
     } catch (error) {
@@ -25,36 +43,40 @@ function Card() {
   }, []);
 
   return (
-    <>
+    <div className="container flex flex-wrap justify-center gap-2">
+      {product.menu_principal &&
+        product.menu_principal.map((principal) => (
+          <Principal key={principal.id} principal={principal} />
+        ))}
+      {product.menu_infantil &&
+        product.menu_infantil.map((infantil) => (
+          <Infantil key={infantil.id} infantil={infantil} />
+        ))}
       {product.postre &&
         product.postre.map((postre) => (
-          <div
-            className="container flex items-center justify-center min-h-screen mx-auto"
-            key={postre.id}
-          >
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-              <div className="card ">
-                <div className="flex flex-col p-5">
-                  <div className="overflow-hidden rounded-xl">
-                    <img className= "object-contain center" src={postre.image} alt={postre.name}></img>
-                  </div>
-                  <h5 className="mt-3 text-2xl font-medium md:text-3xl">
-                    {postre.name}
-                  </h5>
-                  <p className="mt-3 text-lg text- text-slate-500">
-                    {`$ ${postre.price}`}
-                  </p>
-                  <CardButton />
-                  <CardButtonAdd />
-                </div>
-
-              </div>
-              
-            </div>
-          </div>
+          <Postre key={postre.id} postre={postre} />
         ))}
-    </>
-    
+      {product.acompaniamiento &&
+        product.acompaniamiento.map((acompaniamiento) => (
+          <Acompaniamiento key={acompaniamiento.id} acompaniamiento={acompaniamiento} />
+        ))}
+      {product.cafeteria &&
+        product.cafeteria.map((cafeteria) => (
+          <Cafeteria key={cafeteria.id} cafeteria={cafeteria} />
+        ))}
+      {product.bebidas &&
+        product.bebidas.map((bebida) => (
+          <Bebidas key={bebida.id} bebida={bebida} />
+        ))}
+      {product.cervezas &&
+        product.cervezas.map((cerveza) => (
+          <Cervezas key={cerveza.id} cerveza={cerveza} />
+        ))}
+      {product.vinos &&
+        product.vinos.map((vino) => (
+          <Vinos key={vino.id} vino={vino} />
+        ))}
+    </div>
   );
 }
 
