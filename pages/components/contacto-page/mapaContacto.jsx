04b39ef20bfbdab5905c1f6f0f29bb9e393d9bfb
Florencia <biasiflorenciaf@gmail.com>
2023-10-09 17:59:42 -0300

    Se agregó validación de formularioContact y se modificaron estilos en mapaContacto

--- pages/components/contacto-page/mapaContacto.jsx
+++ pages/components/contacto-page/mapaContacto.jsx
@@ -6,21 +6,21 @@ import Image from "next/image";
 export default function MapaContacto() {
   return (
     <section className="grid grid-cols-3 place-content-cente grid-rows-3 gap- grid-rows-1fr">
-      <article className="col-span-3 row-1">
-        <div className="flex gap-4 relative bottom-7 w-[270px] ">
+      <article className="col-span-3 row-1 p-3">
+        <div className="flex gap-5 relative bottom-8 mr-12 w-[270px] ">
           <Image src={location} alt="location" />
           <p className="text-gray-50 text-md relative flex-row left-9 grid place-content-center ">
             Rodríguez Peña 684, CABA
           </p>
         </div>
         <div className="flex gap-5  relative bottom-2 ">
-          <Image src={wasap} alt="wasap" />
+          <Image src={wasap} alt="wasap" className="rounded-full shadow-md shadow-gray-600 bg-[#EBAA10]  p-2" />
           <p className="text-gray-50 text-md relative left-10 grid place-content-center ">
             +54116509428
           </p>
         </div>
-        <div className="flex gap-5  relative top-5  ">
-          <Image src={email} alt="email" />
+        <div className="flex gap-5  relative top-4  ">
+          <Image src={email} alt="email" className="rounded-full shadow-md shadow-gray-600 bg-[#EBAA10]  p-2" />
           <p className="text-gray-50 text-ellipsis text-md relative left-9 grid place-content-center">
             info@lasbrasascook.com
           </p>
@@ -36,9 +36,9 @@ export default function MapaContacto() {
 
       <article className="col-span-3 row-3 p-5 grid place-content-center ">
         <div className="relative right-11">
-          <p className="text-gray-50 text-center text-md ">horarios</p>
+          <p className="text-gray-50 text-center text-md ">Horarios</p>
           <p className="text-gray-50 text-ellipsis text-md ">
-            martes a domingos
+            Martes a Domingos
           </p>
           <p className="text-gray-50 text-ellipsis text-md ">
             12:00hs - 16:00hs
