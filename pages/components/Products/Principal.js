4adba11d1d735a5e05d5965839f0603fbca57a81
facu <facundo19burgos@gmail.com>
2023-10-10 03:46:53 -0300

    update tag image

--- pages/components/Products/Principal.js
+++ pages/components/Products/Principal.js
@@ -1,16 +1,17 @@
 import CardButton from "../Card/CardButton";
 import CardButtonAdd from "../Card/CardButtonAdd";
+import Image from "next/image";
 
 function Principal({ principal }) {
   return (
     <div className="flex items-center justify-center my-28 md: w-80 h-auto">
     <div className="flex flex-col p-5 h-100">
       <div className="overflow-hidden rounded-xl">
-        <img
+        <Image
           className="object-contain center"
           src={ principal.image }
           alt={ principal.name }
-        ></img>
+        />
       </div>
       <h5 className="mt-3 text-2xl font-medium md:text-3xl">
         {principal.name}
