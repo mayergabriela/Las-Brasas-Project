0bbe4c6a46b2002cb873f35d8e5ad5a4cbf3f003
Florencia <biasiflorenciaf@gmail.com>
2023-10-24 04:16:08 -0300

    Se modificaron images en Carousel component, se agregó validación en componente formularioContact

--- pages/components/contacto-page/formularioContact.jsx
+++ pages/components/contacto-page/formularioContact.jsx
@@ -1,26 +1,98 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useState } from "react";
+
 export default function FormularioContacto() {
+  const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
   return (
     <div>
       <div className="text-white text-center py-4">
         <h1 className="text-2xl font-bold pb-4">Envianos un mensaje</h1>
         <p className="font-thin text-xl ">Nos encantaría saber que tenes para decirnos</p>
       </div>
-      <form>
-        <div className="flex flex-col py-2">
-          <input className="border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black" type="text" placeholder="Nombre y Apellido"></input>
-        </div>
-        <div className="flex flex-col py-2">
-          <input className="border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black" type="email" placeholder="Email"></input>
-        </div> 
-        <div className="flex flex-col py-2">
-          <input className="border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black" type="text" placeholder="Asunto"></input>
-        </div>
-        <div className="flex flex-col py-2">
-          <textarea className="resize-none border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black" rows={5} placeholder="Mensaje" resize="none"></textarea>
-        </div>
-        <button className=" text-white bg-[#EBAA10] hover:bg-[#2c2c2c] hover:text-[#EBAA10] font-bold w-full mt-4 rounded-full text-md md:text-xl px-6 py-2">ENVIAR</button>
-      </form>
+      <Formik
+        initialValues={{
+          nombre: '',
+          email: '',
+          asunto: '',
+          mensaje: ''
+        }}
+        validate={(valores) => {
+          let errores = {};
+
+          //Validación nombre:
+          if (!valores.nombre) {
+            errores.nombre = 'Por favor ingresá tu nombre';
+          } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)) {
+            errores.nombre = 'El nombre solo puede contener letras y espacios.'
+          }
+
+          //Validación email:
+          if (!valores.email) {
+            errores.email = 'Por favor ingresá tu email';
+          } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)) {
+            errores.email = 'El email solo puede contener letras, números, puntos, guíones y guión bajo.'
+          }
+
+          return errores;
+        }}
+        onSubmit={(valores, {resetForm}) => {
+          resetForm();
+          console.log('Formulario envíado');
+          cambiarFormularioEnviado(true);
+          setTimeout(() => cambiarFormularioEnviado(false), 3000);
+        }} 
+      >
+        {( { errors } ) => (
+          <Form>
+            <div className="flex flex-col py-2">
+              <Field 
+                className="border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black"
+                id= "nombre"
+                name= "nombre"
+                type="text" 
+                placeholder="Nombre y Apellido" 
+              />
+              <ErrorMessage name="nombre" component={() => (
+                <div className="error text-yellow-500">{errors.nombre}</div>
+              )} />
+            </div>
+            <div className="flex flex-col py-2">
+              <Field 
+                className="border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black" 
+                type="email"
+                id= "email"
+                name= "email"
+                placeholder="Email" 
+              />
+              <ErrorMessage name="email" component={() => (
+                <div className="error text-yellow-500">{errors.email}</div>
+              )} />
+            </div> 
+            <div className="flex flex-col py-2">
+              <Field 
+                className="border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black" 
+                type="text"
+                id= "asunto"
+                name= "asunto"
+                placeholder="Asunto" 
+              />
+            </div>
+            <div className="flex flex-col py-2">
+              <Field as= "textarea"
+                className="border-2 rounded-lg p-3 flex border-gray-300 text-sm text-black resize-none" 
+                rows={5}
+                id="mensaje"
+                name= "mensaje" 
+                placeholder="Mensaje" 
+              />
+            </div>
+            <button className=" text-white bg-[#EBAA10] hover:bg-[#2c2c2c] hover:text-[#EBAA10] font-bold w-full mt-4 rounded-full text-md md:text-xl px-6 py-2" type="submit">ENVIAR</button>
+            {formularioEnviado && <p className="text-green-700 font-bold text-center m-4">¡Formulario envíado con éxito!</p>}
+          </Form>
+        )} 
+      </Formik>  
     </div>
     
   );    
 };
+
